ac878cc2398d61aa85e2119d78bdbeb93fac9910
Julia Luo <juliazhiyuan.luo@gmail.com>
2016-12-31 17:36:46 -0500

    added register function

--- personalize-login/personalize-login.php
+++ personalize-login/personalize-login.php
@@ -16,7 +16,19 @@ class Personalize_Login_Plugin {
      * hooks in the constructor.
      */
     public function __construct() {
+        //login page
         add_shortcode( 'custom-login-form', array( $this, 'render_login_form' ) );
+        add_action( 'login_form_login', array( $this, 'redirect_to_custom_login' ) );
+        add_filter( 'authenticate', array( $this, 'maybe_redirect_at_authenticate' ), 101, 3 );
+        add_action( 'wp_logout', array( $this, 'redirect_after_logout' ) );
+        add_filter( 'login_redirect', array( $this, 'redirect_after_login' ), 10, 3 );
+        
+        //registration page
+        add_shortcode( 'custom-register-form', array( $this, 'render_register_form' ) );
+        add_action( 'login_form_register', array( $this, 'redirect_to_custom_register' ) );
+        add_action( 'login_form_register', array( $this, 'do_register_user' ) );
+        add_filter( 'admin_init' , array( $this, 'register_settings_fields' ) );
+        add_action( 'wp_print_footer_scripts', array( $this, 'add_captcha_js_to_footer' ) );
     }
     
     /**
@@ -35,6 +47,10 @@ class Personalize_Login_Plugin {
                 'title' => __( 'Your Account', 'personalize-login' ),
                 'content' => '[account-info]'
             ),
+            'member-register' => array(
+            'title' => __( 'Register', 'personalize-login' ),
+            'content' => '[custom-register-form]'
+    ),
         );
     
         foreach ( $page_definitions as $slug => $page ) {
@@ -83,6 +99,23 @@ class Personalize_Login_Plugin {
             $attributes['redirect'] = wp_validate_redirect( $_REQUEST['redirect_to'], $attributes['redirect'] );
         }
         
+        // Error messages
+        $errors = array();
+        if ( isset( $_REQUEST['login'] ) ) {
+            $error_codes = explode( ',', $_REQUEST['login'] );
+        
+            foreach ( $error_codes as $code ) {
+                $errors []= $this->get_error_message( $code );
+            }
+        }
+        $attributes['errors'] = $errors;
+        
+        // Check if user just logged out
+        $attributes['logged_out'] = isset( $_REQUEST['logged_out'] ) && $_REQUEST['logged_out'] == true;
+        
+        // Check if the user just registered
+        $attributes['registered'] = isset( $_REQUEST['registered'] );
+        
         // Render the login form using an external template
         return $this->get_template_html( 'login-form', $attributes );
     }
@@ -112,6 +145,366 @@ class Personalize_Login_Plugin {
         ob_end_clean();
     
         return $html;
+    }
+    
+    /**
+    * Redirect the user to the custom login page instead of wp-login.php.
+    */
+    function redirect_to_custom_login() {
+        if ( $_SERVER['REQUEST_METHOD'] == 'GET' ) {
+            $redirect_to = isset( $_REQUEST['redirect_to'] ) ? $_REQUEST['redirect_to'] : null;
+        
+            if ( is_user_logged_in() ) {
+                $this->redirect_logged_in_user( $redirect_to );
+                exit;
+            }
+    
+            // The rest are redirected to the login page
+            $login_url = home_url( 'member-login' );
+            if ( ! empty( $redirect_to ) ) {
+                $login_url = add_query_arg( 'redirect_to', $redirect_to, $login_url );
+            }
+    
+            wp_redirect( $login_url );
+            exit;
+        }
+    }
+    
+    /**
+    * Redirects the user to the correct page depending on whether he / she
+    * is an admin or not.
+    *
+    * @param string $redirect_to   An optional redirect_to URL for admin users
+    */
+    private function redirect_logged_in_user( $redirect_to = null ) {
+        $user = wp_get_current_user();
+        if ( user_can( $user, 'manage_options' ) ) {
+            if ( $redirect_to ) {
+                wp_safe_redirect( $redirect_to );
+            } else {
+                wp_redirect( admin_url() );
+            }
+        } else {
+            wp_redirect( home_url( 'member-account' ) );
+        }
+    }
+    
+    /**
+    * Redirect the user after authentication if there were any errors.
+    *
+    * @param Wp_User|Wp_Error  $user       The signed in user, or the errors that have occurred during login.
+    * @param string            $username   The user name used to log in.
+    * @param string            $password   The password used to log in.
+    *
+    * @return Wp_User|Wp_Error The logged in user, or error information if there were errors.
+    */
+    function maybe_redirect_at_authenticate( $user, $username, $password ) {
+        // Check if the earlier authenticate filter (most likely,ge 
+        // the default WordPress authentication) functions have found errors
+        if ( $_SERVER['REQUEST_METHOD'] === 'POST' ) {
+            if ( is_wp_error( $user ) ) {
+                $error_codes = join( ',', $user->get_error_codes() );
+    
+                $login_url = home_url( 'member-login' );
+                $login_url = add_query_arg( 'login', $error_codes, $login_url );
+    
+                wp_redirect( $login_url );
+                exit;
+            }
+        }  
+        return $user;
+    }
+    
+    /**
+    * Finds and returns a matching error message for the given error code.
+    *
+    * @param string $error_code    The error code to look up.
+    *
+    * @return string               An error message.
+    */
+    private function get_error_message( $error_code ) {
+        switch ( $error_code ) {
+            case 'empty_username':
+                return __( 'Please enter an email.', 'personalize-login' );
+    
+            case 'empty_password':
+                return __( 'Please enter a password.', 'personalize-login' );
+    
+            case 'invalid_username':
+                return __(
+                    "We don't have any users with that email address. Please try again.", 'personalize-login' );
+    
+            case 'incorrect_password':
+                $err = __(
+                    "The password you entered wasn't quite right. <a href='%s'>Did you forget your password</a>?",
+                    'personalize-login'
+                );
+                return sprintf( $err, wp_lostpassword_url() );
+            
+            // Registration errors
+            case 'email':
+                return __( 'The email address you entered is not valid.', 'personalize-login' );
+            
+            case 'email_exists':
+                return __( 'An account exists with this email address.', 'personalize-login' );
+            
+            case 'closed':
+                return __( 'Registering new users is currently not allowed.', 'personalize-login' );
+            
+            case 'captcha':
+                return __( 'The Google reCAPTCHA check failed. Please try again.', 'personalize-login' );
+    
+            default:
+                break;
+        }
+        return __( 'An unknown error occurred. Please try again later.', 'personalize-login' );
+    }
+    
+    /**
+    * Redirect to custom login page after the user has been logged out.
+    */
+    public function redirect_after_logout() {
+        $redirect_url = home_url( 'member-login?logged_out=true' );
+        wp_safe_redirect( $redirect_url );
+        exit;
+    }
+    
+    /**
+    * Returns the URL to which the user should be redirected after the (successful) login.
+    *
+    * @param string           $redirect_to           The redirect destination URL.
+    * @param string           $requested_redirect_to The requested redirect destination URL passed as a parameter.
+    * @param WP_User|WP_Error $user                  WP_User object if login was successful, WP_Error object otherwise.
+    *
+    * @return string Redirect URL
+    */
+    public function redirect_after_login( $redirect_to, $requested_redirect_to, $user ) {
+        $redirect_url = home_url();
+    
+        if ( ! isset( $user->ID ) ) {
+            return $redirect_url;
+        }
+    
+        if ( user_can( $user, 'manage_options' ) ) {
+            // Use the redirect_to parameter if one is set, otherwise redirect to admin dashboard.
+            if ( $requested_redirect_to == '' ) {
+                $redirect_url = admin_url();
+            } else {
+                $redirect_url = $requested_redirect_to;
+            }
+        } else {
+            // Non-admin users always go to their account page after login
+            $redirect_url = home_url( 'member-account' );
+        }
+    
+        return wp_validate_redirect( $redirect_url, home_url() );
+    }
+    
+    /**
+    * A shortcode for rendering the new user registration form.
+    *
+    * @param  array   $attributes  Shortcode attributes.
+    * @param  string  $content     The text content for shortcode. Not used.
+    *
+    * @return string  The shortcode output
+    */
+    public function render_register_form( $attributes, $content = null ) {
+        // Parse shortcode attributes
+        $default_attributes = array( 'show_title' => false );
+        $attributes = shortcode_atts( $default_attributes, $attributes );
+        
+        // Retrieve possible errors from request parameters
+        $attributes['errors'] = array();
+        if ( isset( $_REQUEST['register-errors'] ) ) {
+            $error_codes = explode( ',', $_REQUEST['register-errors'] );
+        
+            foreach ( $error_codes as $error_code ) {
+                $attributes['errors'] []= $this->get_error_message( $error_code );
+            }
+        }
+        
+        // Retrieve recaptcha key
+        $attributes['recaptcha_site_key'] = get_option( 'personalize-login-recaptcha-site-key', null );
+        
+        if ( is_user_logged_in() ) {
+            return __( 'You are already logged in.', 'personalize-login' );
+        } elseif ( ! get_option( 'users_can_register' ) ) {
+            return __( 'Registering new users is currently not allowed.', 'personalize-login' );
+        } else {
+            return $this->get_template_html( 'register_form', $attributes );
+        }
+    }
+    
+    /**
+    * Redirects the user to the custom registration page instead
+    * of wp-login.php?action=register.
+    */
+    public function redirect_to_custom_register() {
+        if ( 'GET' == $_SERVER['REQUEST_METHOD'] ) {
+            if ( is_user_logged_in() ) {
+                $this->redirect_logged_in_user();
+            } else {
+                wp_redirect( home_url( 'member-register' ) );
+            }
+            exit;
+        }
+    }
+    
+    /**
+    * Handles the registration of a new user.
+    *
+    * Used through the action hook "login_form_register" activated on wp-login.php
+    * when accessed through the registration action.
+    */
+    public function do_register_user() {
+        if ( 'POST' == $_SERVER['REQUEST_METHOD'] ) {
+            $redirect_url = home_url( 'member-register' );
+    
+            if ( ! get_option( 'users_can_register' ) ) {
+                // Registration closed, display error
+                $redirect_url = add_query_arg( 'register-errors', 'closed', $redirect_url );
+            } elseif ( ! $this->verify_recaptcha() ) {
+                // Recaptcha check failed, display error
+                $redirect_url = add_query_arg( 'register-errors', 'captcha', $redirect_url );
+            }  else {
+                $email = $_POST['email'];
+                $first_name = sanitize_text_field( $_POST['first_name'] );
+                $last_name = sanitize_text_field( $_POST['last_name'] );
+    
+                $result = $this->register_user( $email, $first_name, $last_name );
+    
+                if ( is_wp_error( $result ) ) {
+                    // Parse errors into a string and append as parameter to redirect
+                    $errors = join( ',', $result->get_error_codes() );
+                    $redirect_url = add_query_arg( 'register-errors', $errors, $redirect_url );
+                } else {
+                    // Success, redirect to login page.
+                    $redirect_url = home_url( 'member-login' );
+                    $redirect_url = add_query_arg( 'registered', $email, $redirect_url );
+                }
+            }
+    
+            wp_redirect( $redirect_url );
+            exit;
+        }
+    }
+    
+    /**
+    * Validates and then completes the new user signup process if all went well.
+    *
+    * @param string $email         The new user's email address
+    * @param string $first_name    The new user's first name
+    * @param string $last_name     The new user's last name
+    *
+    * @return int|WP_Error         The id of the user that was created, or error if failed.
+    */
+    private function register_user( $email, $first_name, $last_name ) {
+        $errors = new WP_Error();
+    
+        // Email address is used as both username and email. It is also the only
+        // parameter we need to validate
+        if ( ! is_email( $email ) ) {
+            $errors->add( 'email', $this->get_error_message( 'email' ) );
+            return $errors;
+        }
+    
+        if ( username_exists( $email ) || email_exists( $email ) ) {
+            $errors->add( 'email_exists', $this->get_error_message( 'email_exists') );
+            return $errors;
+        }
+    
+        // Generate the password so that the subscriber will have to check email...
+        $password = wp_generate_password( 12, false );
+    
+        $user_data = array(
+            'user_login'    => $email,
+            'user_email'    => $email,
+            'user_pass'     => $password,
+            'first_name'    => $first_name,
+            'last_name'     => $last_name,
+            'nickname'      => $first_name,
+        );
+    
+        $user_id = wp_insert_user( $user_data );
+        wp_new_user_notification( $user_id, $password );
+    
+        return $user_id;
+    }
+    
+    /**
+    * Registers the settings fields needed by the plugin.
+    */
+    public function register_settings_fields() {
+        // Create settings fields for the two keys used by reCAPTCHA
+        register_setting( 'general', 'personalize-login-recaptcha-site-key' );
+        register_setting( 'general', 'personalize-login-recaptcha-secret-key' );
+    
+        add_settings_field(
+            'personalize-login-recaptcha-site-key',
+            '<label for="personalize-login-recaptcha-site-key">' . __( 'reCAPTCHA site key' , 'personalize-login' ) . '</label>',
+            array( $this, 'render_recaptcha_site_key_field' ),
+            'general'
+        );
+    
+        add_settings_field(
+            'personalize-login-recaptcha-secret-key',
+            '<label for="personalize-login-recaptcha-secret-key">' . __( 'reCAPTCHA secret key' , 'personalize-login' ) . '</label>',
+            array( $this, 'render_recaptcha_secret_key_field' ),
+            'general'
+        );
+    }
+    
+    //Add recaptcha fields to general settings
+    public function render_recaptcha_site_key_field() {
+        $value = get_option( 'personalize-login-recaptcha-site-key', '' );
+        echo '<input type="text" id="personalize-login-recaptcha-site-key" name="personalize-login-recaptcha-site-key" value="' . esc_attr( $value ) . '" />';
+    }
+    
+    public function render_recaptcha_secret_key_field() {
+        $value = get_option( 'personalize-login-recaptcha-secret-key', '' );
+        echo '<input type="text" id="personalize-login-recaptcha-secret-key" name="personalize-login-recaptcha-secret-key" value="' . esc_attr( $value ) . '" />';
+    }
+    
+    /**
+    * An action function used to include the reCAPTCHA JavaScript file
+    * at the end of the page.
+    */
+    public function add_captcha_js_to_footer() {
+        echo "<script src='https://www.google.com/recaptcha/api.js'></script>";
+    }
+    
+    /**
+    * Checks that the reCAPTCHA parameter sent with the registration
+    * request is valid.
+    *
+    * @return bool True if the CAPTCHA is OK, otherwise false.
+    */
+    private function verify_recaptcha() {
+        // This field is set by the recaptcha widget if check is successful
+        if ( isset ( $_POST['g-recaptcha-response'] ) ) {
+            $captcha_response = $_POST['g-recaptcha-response'];
+        } else {
+            return false;
+        }
+    
+        // Verify the captcha response from Google
+        $response = wp_remote_post(
+            'https://www.google.com/recaptcha/api/siteverify',
+            array(
+                'body' => array(
+                    'secret' => get_option( 'personalize-login-recaptcha-secret-key' ),
+                    'response' => $captcha_response
+                )
+            )
+        );
+    
+        $success = false;
+        if ( $response && is_array( $response ) ) {
+            $decoded_response = json_decode( $response['body'] );
+            $success = $decoded_response->success;
+        }
+    
+        return $success;
     }     
 }
  
--- personalize-login/templates/login_form.php
+++ personalize-login/templates/login_form.php
@@ -1,1 +1,46 @@
-Sign in
+<div class="login-form-container">
+    <?php if ( $attributes['show_title'] ) : ?>
+        <h2><?php _e( 'Log In', 'personalize-login' ); ?></h2>
+    <?php endif; ?>
+    
+    <!-- Show errors if there are any -->
+    <?php if ( count( $attributes['errors'] ) > 0 ) : ?>
+        <?php foreach ( $attributes['errors'] as $error ) : ?>
+            <p class="login-error">
+                <?php echo $error; ?>
+            </p>
+        <?php endforeach; ?>
+    <?php endif; ?>
+    
+    <!-- Show logged out message if user just logged out -->
+    <?php if ( $attributes['logged_out'] ) : ?>
+        <p class="login-info">
+            <?php _e( 'You have logged out. Would you like to log in again?', 'personalize-login' ); ?>
+        </p>
+    <?php endif; ?>
+    
+    <!-- Show message if user just registered -->
+    <?php if ( $attributes['registered'] ) : ?>
+        <p class="login-info">
+            <?php
+                printf(
+                    __( 'You have successfully registered to <strong>%s</strong>. We have emailed your password to the email address you entered. Please change your password after you log in for security purposes.', 'personalize-login' ),
+                    get_bloginfo( 'name' )
+                );
+            ?>
+        </p>
+    <?php endif; ?>
+
+    <?php
+        wp_login_form(
+            array(
+                'label_username' => __( 'Email', 'personalize-login' ),
+                'redirect' => $attributes['redirect'],
+            )
+        );
+    ?>
+     
+    <a class="forgot-password" href="<?php echo wp_lostpassword_url(); ?>">
+        <?php _e( 'Forgot your password?', 'personalize-login' ); ?>
+    </a>
+</div>
